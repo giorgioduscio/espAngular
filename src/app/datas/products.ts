ba94cfdf761b1cb233716b9a9d999769e8e2f984
giorgio <giorgio.duscio@gmail.com>
2024-05-23 13:34:18 +0200

    prodotti

---
+++ src/app/datas/products.ts
@@ -0,0 +1,22 @@
+const products=[
+    {
+      title:"Carlo",
+      subtitle:"Dog breed",
+      URLimage: "https://i.pinimg.com/564x/11/2d/ac/112dacbc4b11dafa67c26f1ac85e72b0.jpg",
+      description: "Bella The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+    {
+      title:"Mario",
+      subtitle:"Dog breed",
+      URLimage: "https://www.repstatic.it/content/contenthub/img/2022/12/29/202146876-79f2b499-4a0d-414d-b61a-515ec4d7f209.jpg",
+      description: "Bella The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+    {
+      title:"Piercarlo",
+      subtitle:"Dog breed",
+      URLimage: "https://cdn-idoli-a.facciabuco.com/273/cane-super-simpa/idolo.jpg?v=0",
+      description: "Bella The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+    },
+  ]
+
+export default products
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+import products from '../datas/products';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule, 
+    MatButtonModule,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
 
+export class ProductsComponent {
+  items=products;
+  // filter='';
 }
