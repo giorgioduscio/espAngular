841a47c4a06988cde65ec143f7d9fb3132ccf0b3
giorgio <giorgio.duscio@gmail.com>
2024-05-23 09:48:56 +0200

    lezione 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    ChiSiamoComponent,
+    DashboardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'chi-siamo', component: ChiSiamoComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
