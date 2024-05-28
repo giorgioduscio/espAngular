47a89439bbf20d33f87ef5ca2549da5aab0a0b0f
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-05-28 10:25:43 +0200

    layout home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
+
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnDestroy{
+[x: string]: any;
 
+  TTT='Titolo'
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  inputValue = '';
+
+  constructor(){
+    console.log('constructor')
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
 }
