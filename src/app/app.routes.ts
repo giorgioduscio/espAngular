2704325076a386d629949204d0a649dc3521c9a2
giorgio <giorgio.duscio@gmail.com>
2024-05-31 12:47:59 +0200

    add shops

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './components/home/home.component';
+import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShopsComponent } from './components/shops/shops.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'chi-siamo', component: ChiSiamoComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'products', component: ProductsComponent },
+    { path: 'shops', component: ShopsComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
