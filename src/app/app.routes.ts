7fb6bd7980ede66de71a97887a4c4bbe8113093c
giorgio <giorgio.duscio@gmail.com>
2024-06-04 13:29:42 +0200

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ShopsComponent } from './components/shops/shops.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -11,5 +12,7 @@ export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'products', component: ProductsComponent },
     { path: 'shops', component: ShopsComponent },
+    { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
+// console.log("da router", routes);
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { routes } from './../../app.routes';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html',
+  imports: [
+    RouterLink,
+  ],
+templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  logged =true
+
+  private R:any =routes;
+  // console.log("router", R)
 }
+
+console.log("router", routes)
