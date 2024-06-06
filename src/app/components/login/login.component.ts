f357b7f00beea4c1babe1861bf85a9d24ccca535
giorgio <giorgio.duscio@gmail.com>
2024-06-06 13:27:20 +0200

    login e logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,59 +1,29 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { User } from '../../interfaces/user';
-import { FormControl, FormGroup, ReactiveFormsModule, Validator, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-  ],
-templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [FormsModule, NgIf],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
 })
 
-export class LoginComponent implements OnInit{
-  constructor(private http: HttpClient){}
-  ngOnInit(): void {
-    this.loginForm =new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-    })
-    throw new Error('Method not implemented.');
-  }
+export class LoginComponent {
+  username: string = 'admin';
+  password: string = 'pa123';
+  errorMessage: string = '';
 
-  url =`http://localhost:3000/api/login`;
-  loginForm: any;
-  errorMessage: string='errore';
-  username: string='admin';
-  password: string='password123';
+  constructor(
+    private http: HttpClient, 
+    private authservice: AuthService,
+  ) {}
 
-  login(){
-    // DATI DA INVIARE
-    const loginData ={
-      username: this.username,
-      password: this.password,
-    }
-
-    // COLLEGAMENTO
-    this.http.post(this.url,loginData).subscribe(
-      {
-        next: (response: any) => {
-          console.log('Login successful:', response);
-
-          // Salva il token nel Local Storage
-          localStorage.setItem('token', response.token);
-
-          // Reindirizza all'applicazione dopo il login
-          window.location.href = '/home';
-        },
-
-        error: (error) => {
-          console.error('Login failed:', error);
-          this.errorMessage = error.message;
-        }
-      }
-    )
+  login(username:string, password:string) {
+    this.authservice.login(username, password)
   }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,27 +1,51 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
 import { routes } from './../../app.routes';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink,
+  RouterLink,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(
+    // private productService :ProductService,
+    private authService :AuthService
+  ){}
+
+  // ACCOUNT
+  token:string |null ='';
+  ngOnInit(): void {
+    this.authService.token$.subscrive((token :any)=>{
+      this.token =token
+    })
+    throw new Error('Method not implemented.');
+  }
+  logout(){
+    this.authService.logout()
+  }
+
+
   @Input() title:any
 
+  // DROPDOWN
   dropdown =false
+  dropdown_f(e:any){
+    if(e.target.className =="dropdownButton") {
+      const div =e.target.children[0]
+      console.log("e", div);
 
-  logged =true
-
-  private R:any =routes;
-  // console.log("router", R)
+      if (div.style.display =="none") div.style.display =="flex"
+      if (div.style.display =="flex") div.style.display =="none"
+    }  
+  }
 }
 
 console.log("router", routes)
