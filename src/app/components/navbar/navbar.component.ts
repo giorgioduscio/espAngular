0e03ee5d35a40bf0c85915d006aa7a063921fead
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-07-11 08:36:01 +0200

    router navbar condizionale

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,49 +3,28 @@ import { Component, Input, OnInit, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { routes } from './../../app.routes';
 import { AuthService } from '../../service/auth.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-  RouterLink,
+    NgFor,
+    NgIf,
+    RouterLink,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
-export class NavbarComponent implements OnInit {
-  constructor(
-    // private productService :ProductService,
-    private authService :AuthService
-  ){}
-
-  // ACCOUNT
-  token:string |null ='';
-  ngOnInit(): void {
-    this.authService.token$.subscrive((token :any)=>{
-      this.token =token
-    })
-    throw new Error('Method not implemented.');
-  }
-  logout(){
-    this.authService.logout()
+export class NavbarComponent {
+  routerClone =routes
+  constructor( private authService :AuthService ){
+    // console.log(`this.routerClone`, this.routerClone);
+    
   }
 
 
   @Input() title:any
 
-  // DROPDOWN
-  dropdown =false
-  dropdown_f(e:any){
-    if(e.target.className =="dropdownButton") {
-      const div =e.target.children[0]
-      console.log("e", div);
-
-      if (div.style.display =="none") div.style.display =="flex"
-      if (div.style.display =="flex") div.style.display =="none"
-    }  
-  }
 }
-
-console.log("router", routes)
