7d522096f8179c373ff264937d87d3e35839917e
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-07-12 10:13:57 +0200

    users dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import users from '../../datas/users';
-import { User } from '../../interfaces/user';
 import { filterUser } from '../../pipes/filter-user.pipe';
 import { FormsModule } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +10,22 @@ import { FormsModule } from '@angular/forms';
   imports: [
     filterUser,
     FormsModule,
+    NgFor, NgIf,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  users:User[]= users;
-  filter:string= ""
+export class DashboardComponent{
+  users :any[] =[]
+  userKeys :any[] =[]
+  constructor(private usersService: UsersService){
+    this.usersService.getUsers().subscribe(data=> {
+      this.users =data
+      // MEMORIZZA SOLO LE CHIAVI CHE NON SONO OGGETTI
+      this.userKeys =Object.keys(this.users[0])
+        .filter(key =>typeof(this.users[0][key])!="object")
+
+      console.log("users", this.users, this.userKeys);
+    })
+  }
 }
