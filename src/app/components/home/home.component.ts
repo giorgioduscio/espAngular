3bb860bb63cfe4c2bddeb2b6212d03ba3958cb9c
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-07-19 13:48:01 +0200

    .

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,10 +5,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { CommonModule, NgFor, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { HomeService } from '../../services/home.service';
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-home',
